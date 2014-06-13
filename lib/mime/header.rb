55ff4128152f7ba5f36dcc9397d9ae67aec3b397
Thorsten von Eicken <tve@rightscale.com>
2014-06-13 12:51:16 -0700

    Revert "ensure Content-Type header comes first"
    This reverts commit 40319c4ba8f6b194d5d221d549a3b524911d970d.

--- lib/mime/header.rb
+++ lib/mime/header.rb
@@ -17,11 +17,7 @@ module MIME
     # word.scan(/(.,?){1,78}/) OR word.split
     #
     def to_s
-      # For our purposes the Content-Type header must come first
-      ct = @headers.find {|k,v| k.downcase == "content-type"}
-      h = @headers.reject{|k,v| k.downcase == "content-type"}
-                  .to_a.map {|kv| kv.join(": ")}.join("\r\n")
-      ct ? ct.join(": ") + "\r\n" + h : h
+      @headers.to_a.map {|kv| kv.join(": ")}.join("\r\n")
     end
 
     #
